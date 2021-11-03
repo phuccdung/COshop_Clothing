4b115149c769550c9d1946858efba3c4aee1499f
phuccdung <82568398+phuccdung@users.noreply.github.com>
2021-11-03 22:11:22 +0700

    API register/login/update user

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv= require('dotenv');
+const authRoute=require("./routes/auth");
+const userRoute=require("./routes/user");
+
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+).then(()=>console.log("DBConnect"))
+.catch((err)=>{
+    console.log("err");
+});
+app.use(express.json());
+
+app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
+
+app.listen(process.env.PORT || 5000,()=>{
+    console.log('Backend server is running')
+});
