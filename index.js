82b1ef16dd6240bbb02b90b8ab1919feafab1051
phuccdung <82568398+phuccdung@users.noreply.github.com>
2021-11-06 22:29:51 +0700

    done order/income monthly

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const authRoute=require("./routes/auth");
 const userRoute=require("./routes/user");
 const productRoute=require("./routes/product");
 const cartRoute=require("./routes/cart");
+const orderRoute=require("./routes/order");
+
 
 
 
@@ -23,7 +25,9 @@ app.use(express.json());
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
 app.use("/api/products",productRoute);
-app.use("/api/cart",cartRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/orders",orderRoute);
+
 
 
 
