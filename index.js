b8d54d045826d179f8cc264d9df59fb39c529f7c
phuccdung <82568398+phuccdung@users.noreply.github.com>
2021-11-06 22:01:04 +0700

    done API product,cart

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const dotenv= require('dotenv');
 const authRoute=require("./routes/auth");
 const userRoute=require("./routes/user");
+const productRoute=require("./routes/product");
+const cartRoute=require("./routes/cart");
+
+
 
 
 dotenv.config();
@@ -18,6 +22,10 @@ app.use(express.json());
 
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
+app.use("/api/products",productRoute);
+app.use("/api/cart",cartRoute);
+
+
 
 app.listen(process.env.PORT || 5000,()=>{
     console.log('Backend server is running')
