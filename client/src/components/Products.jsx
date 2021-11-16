597985ac704f3ea626584ca21dc38ef8ac4bd066
phuccdung <82568398+phuccdung@users.noreply.github.com>
2021-11-16 16:12:26 +0700

    get product by cat and filter

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { popularProducts } from "../data";
 import Product from "./Product";
 import {useEffect, useState} from "react";
 import axios from "axios";
@@ -16,21 +15,52 @@ const Products = ({cat,filters,sort}) => {
   const [products,setProducts]=useState([]);
   const [filteredProducts,setFilteredProducts]=useState([]);
   useEffect(()=>{
-    const getProducts=async ()=>{
+    const getProducts =async ()=>{
       try{
-        const res =await axios.get("http://localhost:5000/api/products");
-        console.log(res);
+        const res =await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}`:"http://localhost:5000/api/products");
+        setProducts(res.data);
       }catch(err){}
     };
     getProducts();
   },[cat]);
  
+  useEffect(()=>{
+    cat && 
+          setFilteredProducts(
+            products.filter((item)=>
+              Object.entries(filters).every(([key,value])=>
+                item[key].includes(value)
+              )
+            )
+          );
+  },[products,cat,filters]);
+
+  useEffect(()=>{
+    if(sort==="newest"){
+      setFilteredProducts(prev=>
+        [...prev].sort((a,b)=>a.createdAt-b.createdAt)
+        );
+    } else{
+      if(sort==="asc"){
+        setFilteredProducts(prev=>
+          [...prev].sort((a,b)=>a.price-b.price)
+          );
+      } else {
+        setFilteredProducts(prev=>
+          [...prev].sort((a,b)=>b.price-a.price)
+          );
+      }
+    }
+  },[sort]);
 
   return (
     <Container>
-      {popularProducts.map((item) => (
-        <Product item={item} key={item.id} />
-      ))}
+      {cat 
+        ?filteredProducts.map((item) => (<Product item={item} key={item.id} />))
+        :products
+          .slice(0,8)
+          .map((item) => (<Product item={item} key={item.id} />))
+    }
     </Container>
   );
 };
