c9c2444fd00b382c53a70268d79c1482f540d068
phuccdung <82568398+phuccdung@users.noreply.github.com>
2021-11-22 00:37:32 +0700

    add login admin

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,8 +1,21 @@
-const Login=()=>{
+import {useState} from "react";
+
+const Login =()=>{
+
+    const [username,setUsername]=useState("");
+    const [password,setPassword]=useState("");
+
+    const handleClick=(e)=>{
+        e.preventDefault();
+    }
     return(
         <div>
-            <input>
-            </input>
+            <input type="text" placeholder="username" onChange={e=>setUsername(e.target.value)} />
+            <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)} />
+            <button onClick={handleClick}>Login</button>
+
         </div>
     )
-}
+
+};
+export default Login;
